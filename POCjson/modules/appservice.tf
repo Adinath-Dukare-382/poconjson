3cbc67f603629b4562848bd1d4cfdcd93b456b80
Adinath Dukare <Adinath.Dukare@allscripts.com>
2021-10-25 16:55:18 +0530

    added plan id

--- POCjson/modules/appservice.tf
+++ POCjson/modules/appservice.tf
@@ -4,5 +4,5 @@ resource "azurerm_app_service" "example" {
   name                  = format("app-%s-%s",each.value.app_name,each.value.version)
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = var.plan_name.app_service_plan_id
+  app_service_plan_id = var.plan_id
 }
