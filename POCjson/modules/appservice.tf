ca33a2dbd0ea0864f31aa870bdfc6fd518a23289
Adinath Dukare <Adinath.Dukare@allscripts.com>
2021-10-25 16:43:24 +0530

    update name of app service

--- POCjson/modules/appservice.tf
+++ POCjson/modules/appservice.tf
@@ -1,7 +1,7 @@
 resource "azurerm_app_service" "example" {
 #   name                = "example-app-service"
   for_each              = var.apps
-  name                  = format("app-%s-%s",var.app_name,var.version)
+  name                  = format("app-%s-%s",each.value.app_name,each.value.version)
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = var.plan_name.app_service_plan_id
