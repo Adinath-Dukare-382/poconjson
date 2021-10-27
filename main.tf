4451b664120c31f2e60d3e50fbf65979b025b527
Adinath Dukare <Adinath.Dukare@allscripts.com>
2021-10-27 12:05:09 +0530

    added

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ provider "azurerm" {
 # }
 
 module "name2" {
-  source = "POCJson/modules"
+  source = "./POCJson/modules"
 
   resource_group_name = var.resource_group_name
   location = var.location
